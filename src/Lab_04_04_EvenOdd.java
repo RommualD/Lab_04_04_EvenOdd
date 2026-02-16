ebffd2dfd1cb525f174c0b72fa62d82097e7c302
RommualD <rommualdesrosiers@gmail.com>
2026-02-16 13:00:26 -0500

    Initial commit

---
+++ src/Lab_04_04_EvenOdd.java
@@ -0,0 +1,14 @@
+public class Lab_04_04_EvenOdd {
+    public static void main(String[] args) {
+
+        int numToExamine = 2;
+        int remainder = numToExamine % 2;
+        System.out.println("The number to examine is: " + numToExamine);
+
+        if (remainder == 0) {
+            System.out.println(numToExamine + " is Even because " + numToExamine + " % 2 = 0");
+        } else {
+            System.out.println(numToExamine + " is Odd because " + numToExamine + " % 2 = 1");
+        }
+    }
+}
